9e05d1a132bc0e7c356494e724a6db612ad27f68
NajibHossain49 <najibhossain1234@gmail.com>
2024-11-02 13:16:21 +0600

    Project Setup with Dom-Route

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Dynamic Section */}
+      <div className="min-h-[calc(100vh-232px)] py-12">
+        <Outlet />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom'
+import MainLayout from '../Layouts/MainLayout';
+const routes = createBrowserRouter([
+
+{
+    path: "/",
+    element: <MainLayout />,
+}
+
+  
+]);
+
+
+export default routes;
